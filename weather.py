167417c6a5081f188bf574e9574c977e728c2eef
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2021-05-20 14:07:10 +0530

    Created a basic window of required dimensions and added a title.

---
+++ weather.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import requests
+import time
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.geometry("600x500")
+    window.title("PyWeather")
+
+    window.mainloop()
