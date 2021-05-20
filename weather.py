306794a8b2e6b1243592d779ddc87e2f1f4c826b
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2021-05-20 14:10:51 +0530

    Created a text field where user can write the city name without moving the cursor at all.

--- weather.py
+++ weather.py
@@ -8,4 +8,16 @@ if __name__ == "__main__":
     window.geometry("600x500")
     window.title("PyWeather")
 
+    f = ("ubuntu", 15, "bold")
+    t = ("ubuntu", 35, "bold")
+
+    textField = tk.Entry(window, font=t)
+    textField.pack(pady=20)
+    textField.focus()
+
+    label1 = tk.Label(window, font=t)
+    label1.pack()
+    label2 = tk.Label(window, font=f)
+    label2.pack()
+
     window.mainloop()
